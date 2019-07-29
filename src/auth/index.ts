b2f10a9d69ffd99aac7a7f090d9c61ab43de5245
daniel <schmitt@sd-softdev.de>
2019-07-29 16:39:08 +0200

    #REMOVED unnecessary files

--- src/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export { JwtStrategy } from './jwt.strategy';
-export { jwtConstants } from './constant';
-export { LocalStrategy } from './local.strategy';
-export { AuthModule } from './auth.module';
--- src/user/user.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { User.Entity } from './user.entity';
-
-describe('User.Entity', () => {
-  it('should be defined', () => {
-    expect(new User.Entity()).toBeDefined();
-  });
-});
